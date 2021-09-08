c48984180647c0d34c1d5c139c127d7a3903769c
lorenzokata <catalanolorenzo95@gmail.com>
2021-09-08 17:01:08 +0200

    migration vacation_packages table

---
+++ database/migrations/2021_09_08_135646_create_vacation_packages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVacationPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacation_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('travel_agency', 80);
+            $table->tinyInteger('travelers')->unsigned();
+            $table->tinyInteger('days')->unsigned();
+            $table->string('location', 100);
+            $table->string('transportation', 30);
+            $table->string('property', 30);
+            $table->date('departing');
+            $table->date('returning');
+            $table->text('details')->nullable();
+            $table->float('discount', 5, 2)->nullable();
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacation_packages');
+    }
+}
